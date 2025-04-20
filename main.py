545aa569b590d1c091e6cb5785453f437439a875
UjnsMedia <ujnsmedia@gmail.com>
2025-04-20 23:07:59 +0530

    added url to  cors origin url

--- main.py
+++ main.py
@@ -43,10 +43,9 @@ app = FastAPI(
 
 origins = [
     # add your cors origin address and portno here
-    "https://satzurajkumar.github.io/demo-frontend-for-semantic-search/",
     "https://satzurajkumar.github.io",
-    "http://satzurajkumar.github.io"
-    "https://search-rndr.onrender.com",
+
+    # Origins for local development (if needed)
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     
