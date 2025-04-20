32c7c438c6d71b39eccdbec8c74935797851cd35
UjnsMedia <ujnsmedia@gmail.com>
2025-04-20 20:22:57 +0530

    cors url update

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ app = FastAPI(
 
 origins = [
     # add your cors origin address and portno here
-    "https://satzurajkumar.github.io/"
-    # "http://127.0.0.1:5500",
-    # "http://localhost:5500",
+    "https://satzurajkumar.github.io/",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     
 ]
 
